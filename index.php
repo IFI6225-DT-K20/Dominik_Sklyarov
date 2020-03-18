46e100506a7a9b333187fb98507896ff1c73fa0b
Dominik <dominik@sklyarov.us>
2020-03-18 13:43:56 +0200

    landing page development

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Startup Lisboa</title>
+    <?php include('templates/header.php') ?>
+<body>
+<div class="header">
+    <div class="header-container">
+        <div>
+            <div class="page-logo">
+                <img src="assets/images/logo/Logo_SULx_v2.png" alt="">
+            </div>
+        </div>
+        <div class="page-links">
+            <nav>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <button class="dropbtn">About</button>
+                            <div class="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <button class="dropbtn">Community</button>
+                            <div class="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        Jobs
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <button class="dropbtn">Events</button>
+                            <div class="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <button class="dropbtn">Programs</button>
+                            <div class="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        JVAwards
+                    </li>
+                    <li>
+                        Hub creativo beato
+                    </li>
+                    <li>
+                        newsroom
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <button class="dropbtn">Facilities</button>
+                            <div class="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        apply
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+<div class="homepage-landing">
+    <div class="homepage-info">
+        <h1 class="page-heading">Lisbon's Incubator</h1>
+        <p class="page-subtitle">Startup Lisboa supports the creation of companies and their first years of activity.
+            Founded in 2012 it's a
+            private non-profit association that provides entrepreneurs with office space as well as a support structure
+            - mentoring, strategic partnerships and perks, access to investment, networking activities and a community
+            based on knowledge and sharing.</p>
+    </div>
+    <div class="scrolldown-container">
+        <img src="assets/images/icons/scrolldown.svg" alt="">
+    </div>
+</div>
+</body>
+</html>
